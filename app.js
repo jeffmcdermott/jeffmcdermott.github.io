d684dc347f0fbae6e36c6d522a335f653a91cba9
Jeff McDermott <jeff.mcdermott@gmail.com>
2019-02-15 16:54:01 -0600

    added basic service worker

---
+++ app.js
@@ -0,0 +1,11 @@
+(function() {
+'use strict';
+
+//register service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./sw.js')
+    .then(function() { console.log('Service Worker Registered'); });
+}
+
+})();
---
+++ sw.js
@@ -0,0 +1,49 @@
+var cacheName = 'waysecret-cache-1'; //version me on new releases
+
+var filesToCache = [
+  '/',
+ "./index.html"
+];
+
+//INSTALL
+self.addEventListener('install', function(e) {
+  console.log('Service Worker Installing ' + cacheName + '...');
+
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('Service Worker ' + cacheName + ' Cache Online ...');
+      return cache.addAll(filesToCache);
+    })
+  );
+
+});
+
+//ACTIVATE
+self.addEventListener('activate', function(e) {
+  console.log('Service Worker Activating ' + cacheName + '...');
+
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('Service Worker Removing ' + key + ' Cache');
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+
+  //claim unclaimed clients now
+  return self.clients.claim();
+});
+
+//FETCH
+self.addEventListener('fetch', function(fetchEvent) {
+  console.log('Service Worker Fetching: ', fetchEvent.request.url);
+
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
