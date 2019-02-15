6426c8a46b55a39c334a668bd0d80384356fb485
Jeff McDermott <jeff.mcdermott@gmail.com>
2019-02-15 17:21:50 -0600

    added manifest

--- sw.js
+++ sw.js
@@ -2,9 +2,16 @@ var cacheName = 'waysecret-cache-1'; //version me on new releases
 
 var filesToCache = [
   '/',
- "./index.html",
- "./app.js",
- "./sw.js"
+  "./index.html",
+  "./app.js",
+  "./sw.js",
+  "./images",
+  "./images/icons",
+  "./images/icons/icon-128x128.png",
+  "./images/icons/icon-144x144.png",
+  "./images/icons/icon-152x152.png",
+  "./images/icons/icon-192x192.png",
+  "./images/icons/icon-256x256.png"
 ];
 
 //INSTALL
